dfd6d9e1d622ecf0dcf078bfc1ebacb200edb8c2
Vasudeva <sharma91.tangirala@gmail.com>
2025-09-08 07:37:17 +0530

    new update

---
+++ resource-dependency/resource-dependency.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region     = "us-east-1"
+  
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0e449927258d45bc4"
+  instance_type = "t2.micro"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "klabs-demo-s3-007"
+}
